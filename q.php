7e4b3e62ad54e96291f9d793ab288bdae6fa7e66
Jake Cattrall <krazyjakee@gmail.com>
2014-03-25 23:03:20 +0000

    use php to read/write files

---
+++ q.php
@@ -0,0 +1,24 @@
+<?php
+
+	if(isset($_POST['saveAttr'])){
+		$name = $_POST['saveAttr'];
+		$attrs = $_POST['value'];
+
+		file_put_contents("resources/map/".$name.".json", $attrs);
+	}
+	else if(isset($_GET['loadMaps'])){
+		$dir = dirname(__FILE__)."\\resources\\map";
+		$iterator = new DirectoryIterator($dir);
+		$files = [];
+		foreach ($iterator as $fileinfo) {
+		    if ($fileinfo->isFile()) {
+		    	$fname = $fileinfo->getFilename();
+		    	if (strpos($fname, "_attributes") == FALSE){
+		        	array_push($files, $fname);
+		    	}
+		    }
+		}
+		echo json_encode($files);
+	}
+
+?>
