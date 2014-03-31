b309211bfe38b886eefa99a98c4aa2b5a287a287
Jake Cattrall <krazyjakee@gmail.com>
2014-03-31 23:31:40 +0100

    wasd movement

--- coffee/char.coffee
+++ coffee/char.coffee
@@ -1,5 +1,73 @@
 Character = 
 
-	loadPlayer: (sprite) ->
-    elem = $("<div class='player sprite-#{sprite}'></div>").css("background-image", "url(resources/img/#{sprite}.png)")
+  player: 
+    id: 0
+    elem: false
+
+  store: []
+
+  loadPlayer: (sprite) ->
+    id = Character.store.length
+    elem = $("<div class='player sprite-#{sprite} char-#{id}'></div>").css "background-image", "url(resources/img/#{sprite}.png)"
     Map.playerLayer.append elem
+    Character.store.push
+      id: id
+      type: 'player'
+      sprite: sprite
+      top: 0
+      left: 0
+      timer: false
+      direction: ''
+    Character.player.id = id
+    Character.player.elem = $ '.player'
+    Character.setupInput id
+    Character.spriteMover id
+
+  keysDown: ''
+
+  setupInput: (id) ->
+
+    $(document.body).on 'keydown', (e) ->
+      charData = Character.store[id]
+      direction = ''
+      switch e.which
+        when 83 then direction = 's'
+        when 68 then direction = 'd'
+        when 65 then direction = 'a'
+        when 87 then direction = 'w'
+
+      if Character.keysDown.indexOf(direction) is -1
+        Character.keysDown += direction
+
+    $(document.body).on 'keyup', (e) ->
+      charData = Character.store[id]
+      switch e.which
+        when 83
+          Character.keysDown = Character.keysDown.replace(/s/g,'')
+        when 68
+          Character.keysDown = Character.keysDown.replace(/d/g,'')
+        when 65
+          Character.keysDown = Character.keysDown.replace(/a/g,'')
+        when 87
+          Character.keysDown = Character.keysDown.replace(/w/g,'')
+
+  spriteMover: (id) ->
+    Character.store[id].timer = setInterval ->
+      charData = Character.store[id]
+
+      direction = ''
+      direction += 's' if Character.keysDown.indexOf('s') > -1
+      direction += 'd' if Character.keysDown.indexOf('d') > -1
+      direction += 'a' if Character.keysDown.indexOf('a') > -1
+      direction += 'w' if Character.keysDown.indexOf('w') > -1
+
+      for d in direction
+        switch d
+          when 'w' then rules = { top: charData.top-- }
+          when 'a' then rules = { left: charData.left-- }
+          when 's' then rules = { top: charData.top++ }
+          when 'd' then rules = { left: charData.left++ }
+        Character.player.elem.css rules
+
+      charData.direction = direction.split('').sort().join('')
+    , 10
--- js/char.js
+++ js/char.js
@@ -1,9 +1,111 @@
 var Character;
 
 Character = {
+  player: {
+    id: 0,
+    elem: false
+  },
+  store: [],
   loadPlayer: function(sprite) {
-    var elem;
-    elem = $("<div class='player sprite-" + sprite + "'></div>").css("background-image", "url(resources/img/" + sprite + ".png)");
-    return Map.playerLayer.append(elem);
+    var elem, id;
+    id = Character.store.length;
+    elem = $("<div class='player sprite-" + sprite + " char-" + id + "'></div>").css("background-image", "url(resources/img/" + sprite + ".png)");
+    Map.playerLayer.append(elem);
+    Character.store.push({
+      id: id,
+      type: 'player',
+      sprite: sprite,
+      top: 0,
+      left: 0,
+      timer: false,
+      direction: ''
+    });
+    Character.player.id = id;
+    Character.player.elem = $('.player');
+    Character.setupInput(id);
+    return Character.spriteMover(id);
+  },
+  keysDown: '',
+  setupInput: function(id) {
+    $(document.body).on('keydown', function(e) {
+      var charData, direction;
+      charData = Character.store[id];
+      direction = '';
+      switch (e.which) {
+        case 83:
+          direction = 's';
+          break;
+        case 68:
+          direction = 'd';
+          break;
+        case 65:
+          direction = 'a';
+          break;
+        case 87:
+          direction = 'w';
+      }
+      if (Character.keysDown.indexOf(direction) === -1) {
+        return Character.keysDown += direction;
+      }
+    });
+    return $(document.body).on('keyup', function(e) {
+      var charData;
+      charData = Character.store[id];
+      switch (e.which) {
+        case 83:
+          return Character.keysDown = Character.keysDown.replace(/s/g, '');
+        case 68:
+          return Character.keysDown = Character.keysDown.replace(/d/g, '');
+        case 65:
+          return Character.keysDown = Character.keysDown.replace(/a/g, '');
+        case 87:
+          return Character.keysDown = Character.keysDown.replace(/w/g, '');
+      }
+    });
+  },
+  spriteMover: function(id) {
+    return Character.store[id].timer = setInterval(function() {
+      var charData, d, direction, rules, _i, _len;
+      charData = Character.store[id];
+      direction = '';
+      if (Character.keysDown.indexOf('s') > -1) {
+        direction += 's';
+      }
+      if (Character.keysDown.indexOf('d') > -1) {
+        direction += 'd';
+      }
+      if (Character.keysDown.indexOf('a') > -1) {
+        direction += 'a';
+      }
+      if (Character.keysDown.indexOf('w') > -1) {
+        direction += 'w';
+      }
+      for (_i = 0, _len = direction.length; _i < _len; _i++) {
+        d = direction[_i];
+        switch (d) {
+          case 'w':
+            rules = {
+              top: charData.top--
+            };
+            break;
+          case 'a':
+            rules = {
+              left: charData.left--
+            };
+            break;
+          case 's':
+            rules = {
+              top: charData.top++
+            };
+            break;
+          case 'd':
+            rules = {
+              left: charData.left++
+            };
+        }
+        Character.player.elem.css(rules);
+      }
+      return charData.direction = direction.split('').sort().join('');
+    }, 10);
   }
 };
